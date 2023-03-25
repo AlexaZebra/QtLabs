9db17ba8ea56064df7b0d523575e25bd4952a436
Alexa <starovoytova.sasha05@gmail.com>
2023-03-25 21:13:29 +0700

    file creation not working

--- main.cpp
+++ main.cpp
@@ -1,19 +1,35 @@
 #include "FileMonitor.h"
 #include "FileState.h"
+#include "ChangeOutput.h"
 #include <QCoreApplication>
 #include <iostream>
 using namespace std;
 
-void main(){
-    FileMonitor monitor;
-    FileMonitor.AddFile('a.txt');
-    FileMonitor.AddFile('b.txt');
-    FileMonitor.AddFile('c.txt');
-    FileMonitor.AddFile('d.txt');
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Russian");
+    QCoreApplication a(argc, argv);
+    FileMonitor* monitor = new FileMonitor(); // Создаем объект класса FileMonitor
 
 
-    while(doCheckObj){
+    // Устанавливаем соединения между сигналами и слотами
+    QObject::connect(monitor, &FileMonitor::StartMonitor, &ChangeOutput::logStartMonitor);
+    QObject::connect(monitor, &FileMonitor::checkCreated, &ChangeOutput::logCreate);
+    QObject::connect(monitor, &FileMonitor::checkChanged, &ChangeOutput::logChange);
+    QObject::connect(monitor, &FileMonitor::checkDeleted, &ChangeOutput::logDelete);
 
-    FileMonitor.CheckStatus();
+    // Добавляем файлы для отслеживания
+    monitor->addFile("C:\\Users\\staro\\Lab_1\\files\\test1.txt");
+    monitor->addFile("C:\\Users\\staro\\Lab_1\\files\\test2.txt");
+    monitor->addFile("C:\\Users\\staro\\Lab_1\\files\\test3.txt");
+    monitor->addFile("C:\\Users\\staro\\Lab_1\\files\\test4.txt");
 
-};
+    // Запустим цикл котрый будет постоянно опрашивать изменения в файлах
+        while (1) {
+            monitor->updateFileState();
+        }
+
+    return a.exec();
+
+}
